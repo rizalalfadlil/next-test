adaff163652d426569397411667bdad63f4131c8
RizalAlFadlil <rizalalfadlil@gmail.com>
2024-04-07 17:13:04 +0700

    error

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,19 +1,37 @@
 /* eslint-disable react/jsx-key */
-"use client"
-import LayoutBase from '@/components/layout'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import config from '../../config.json'
+"use client";
+import LayoutBase from "@/components/layout";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import config from "../../config.json";
+
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table";
-import { Button } from '@/components/ui/button'
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import dayjs from "dayjs";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { CalendarSearch } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+const showedFormat = "DD MMMM YYYY";
 
 const formatRupiah = (number: number) => {
   return Intl.NumberFormat("id-ID", {
@@ -23,61 +41,172 @@ const formatRupiah = (number: number) => {
 };
 
 const navigateTo = (url: string) => {
-    window.location.href = url;
-  };
+  window.location.href = url;
+};
 
 export default function OrderList() {
-  const userData = localStorage.getItem('user')
-  const parsedUser:{id:string, username:string, name:string, type:string} = JSON.parse(userData || '{}')
-    const [data, setData] = useState([])
-    const getOrderData = async ()=>{
-        try{
-            const res = await axios.get(`${config.db}api/collections/orders/records`)
-            setData(res.data.items)
-            console.log(res.data)
-        }catch(e){
-            console.error(e)
-        }
+  const userData = localStorage.getItem("user");
+  const parsedUser: {
+    id: string;
+    username: string;
+    name: string;
+    type: string;
+  } = JSON.parse(userData || "{}");
+  const [data, setData] = useState([]);
+  const [date, setDate] = useState<Date | undefined>(undefined);
+  const [users, setUsers] = useState([]);
+  const [filterDate, setFilterDate] = useState([])
+  const [filterUser, setFilterUser] = useState([])
+  const getUsers = async () => {
+    try {
+      const res = await axios.get(
+        `${config.db}api/collections/users/records?page=1&perPage=30`
+      );
+      console.log(res);
+      setUsers(res?.data.items);
+    } catch (e) {
+      console.log(e);
     }
-    useEffect(()=>{
-        getOrderData()
-    },[])
+  };
+  const selectUser = (id: string) => {
+    console.log("selected", id);
+  };
+
+  const selectDate = (date: any) => {
+    setDate(date);
+    console.log("tanggal", dayjs(date).format(showedFormat));
+    // selectUser(selectedUser)
+    setFilterDate(
+      data.filter((d: { id: string; userId: string, created:string }) => {
+        return dayjs(d.created).format(showedFormat) === dayjs(date).format(showedFormat);
+      })
+    )
+  };
+  const getOrderData = async () => {
+    try {
+      const res = await axios.get(`${config.db}api/collections/orders/records`);
+      setData(res.data.items);
+      setFilterDate(res.data.items);
+      console.log(res.data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  useEffect(() => {
+    getUsers();
+    getOrderData();
+  }, []);
+
+  const getMonthIncome = () => {
+    let number: number = 0;
+    const today = new Date();
+    const thisMonth = dayjs(today).get("month");
+    data.map((d: { created: Date; total: string }) => {
+      thisMonth === dayjs(d.created).get("month") &&
+        (number += parseInt(d?.total));
+    });
+    return formatRupiah(number);
+  };
+
+  const getYearIncome = () => {
+    let number: number = 0;
+    const today = new Date();
+    const thisYear = dayjs(today).get("year");
+    data.map((d: { created: Date; total: string }) => {
+      thisYear === dayjs(d.created).get("year") &&
+        (number += parseInt(d?.total));
+    });
+    return formatRupiah(number);
+  };
   return (
     <LayoutBase>
-        <div>OrderList</div>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>id</TableHead>
-              <TableHead>user id</TableHead>
-              <TableHead>pelanggan</TableHead>
-              <TableHead>pesanan</TableHead>
-              <TableHead>total</TableHead>
-              <TableHead>tanggal</TableHead>
-              <TableHead>aksi</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {data?.map(
-              (d: { id: string; userId: string; pelanggan: string, pesanan:string, created:string, total:string }) => {
-                return (
-                  <TableRow>
-                    <TableCell>{d?.id}</TableCell>
-                    <TableCell>{d?.userId}</TableCell>
-                    <TableCell>{d?.pelanggan}</TableCell>
-                    <TableCell>{d?.pesanan}</TableCell>
-                    <TableCell>{formatRupiah(parseInt(d?.total))}</TableCell>
-                    <TableCell>{d?.created.slice(0, -5)}</TableCell>
-                    <TableCell>
-                    <Button onClick={()=>navigateTo(`/order/${d.id}`)}>Lihat</Button>
-                    </TableCell>
-                  </TableRow>
-                );
-              }
-            )}
-          </TableBody>
-        </Table>
-        {parsedUser.type === 'kasir' && <Button className='mt-4' onClick={()=>navigateTo('/order/new')}>Buat pesanan baru</Button>}
+      <div>OrderList</div>
+      <div className="grid grid-cols-3 gap-4 py-4">
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant="outline">
+              <CalendarSearch className="me-4" /> Pilih Tanggal
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent>
+            <Calendar mode="single" selected={date} onSelect={selectDate} />
+          </PopoverContent>
+        </Popover>
+        <Select
+          onValueChange={(e) => {
+            selectUser(e);
+          }}
+        >
+          <SelectTrigger className="w-full">
+            <SelectValue placeholder="pilih user" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {users.map((m: { id: string; name: string }) => (
+                <SelectItem value={m.id}>{m.name}</SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Button variant='outline'>Hapus Filter</Button>
+      </div>
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>id</TableHead>
+            <TableHead>user id</TableHead>
+            <TableHead>pelanggan</TableHead>
+            <TableHead>pesanan</TableHead>
+            <TableHead>total</TableHead>
+            <TableHead>tanggal</TableHead>
+            <TableHead>aksi</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {filterDate?.map(
+            (d: {
+              id: string;
+              userId: string;
+              pelanggan: string;
+              pesanan: string;
+              created: string;
+              total: string;
+            }) => {
+              return (
+                <TableRow>
+                  <TableCell>{d?.id}</TableCell>
+                  <TableCell>{d?.userId}</TableCell>
+                  <TableCell>{d?.pelanggan}</TableCell>
+                  <TableCell>{d?.pesanan}</TableCell>
+                  <TableCell>{formatRupiah(parseInt(d?.total))}</TableCell>
+                  <TableCell>{dayjs(d?.created).format(showedFormat)}</TableCell>
+                  <TableCell>
+                    <Button onClick={() => navigateTo(`/order/${d.id}`)}>
+                      Lihat
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              );
+            }
+          )}
+        </TableBody>
+      </Table>
+      <div className="grid md:grid-cols-2 w-full gap-4 my-8">
+        <div className="border rounded-md p-4 ">
+          <p className="text-lg foont-medium">Pendapatan bulan ini</p>
+          <p className="text-2xl font-bold">{getMonthIncome()}</p>
+        </div>
+        <div className="border rounded-md p-4 ">
+          <p className="text-lg foont-medium">Pendapatan tahun ini</p>
+          <p className="text-2xl font-bold">{getYearIncome()}</p>
+        </div>
+      </div>
+      {parsedUser.type === "kasir" && (
+        <Button className="mt-4" onClick={() => navigateTo("/order/new")}>
+          Buat pesanan baru
+        </Button>
+      )}
     </LayoutBase>
-  )
+  );
 }
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,16 +1,26 @@
 /* eslint-disable react/jsx-key */
-"use client"
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "./ui/accordion";
+import { useTheme } from "next-themes";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
 import { Badge } from "./ui/badge";
-import { Activity, Coffee, LogIn, Menu, NotebookTabs, User } from "lucide-react";
+import {
+  Activity,
+  Coffee,
+  LogIn,
+  Menu,
+  Moon,
+  NotebookTabs,
+  Sun,
+  User,
+} from "lucide-react";
 import { ScrollArea } from "./ui/scroll-area";
 
 const navigateTo = (url: string) => {
@@ -18,67 +28,74 @@ const navigateTo = (url: string) => {
 };
 const adminPage = [
   {
-    title:'Users',
-    target:'users',
-    icon:<User/>
+    title: "Users",
+    target: "users",
+    icon: <User />,
   },
   {
-    title:'Aktivitas',
-    target:'activity',
-    icon:<Activity/>
+    title: "Aktivitas",
+    target: "activity",
+    icon: <Activity />,
   },
   {
-    title:'Log-out',
-    target:'login',
-    icon:<LogIn/>
+    title: "Log-out",
+    target: "login",
+    icon: <LogIn />,
   },
-]
+];
 const managerPage = [
   {
-    title:'menu',
-    target:'menu',
-    icon:<Coffee/>
+    title: "menu",
+    target: "menu",
+    icon: <Coffee />,
   },
   {
-    title:'Aktivitas',
-    target:'activity',
-    icon:<Activity/>
+    title: "Aktivitas",
+    target: "activity",
+    icon: <Activity />,
   },
   {
-    title:'Pesanan',
-    target:'order',
-    icon:<NotebookTabs/>
+    title: "Pesanan",
+    target: "order",
+    icon: <NotebookTabs />,
   },
   {
-    title:'Log-out',
-    target:'login',
-    icon:<LogIn/>
+    title: "Log-out",
+    target: "login",
+    icon: <LogIn />,
   },
-]
+];
 const kasirPage = [
   {
-    title:'Pesanan',
-    target:'order',
-    icon:<NotebookTabs/>
+    title: "Pesanan",
+    target: "order",
+    icon: <NotebookTabs />,
   },
   {
-    title:'Log-out',
-    target:'login',
-    icon:<LogIn/>
+    title: "Log-out",
+    target: "login",
+    icon: <LogIn />,
   },
-]
+];
 export default function LayoutBase({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const userData = localStorage.getItem('user')
-  const parsedUser:{id:string, username:string, name:string, type:string} = JSON.parse(userData || '{}')
+  const {setTheme} = useTheme();
+  const [currentTheme, setCurrentTheme] = useState("light")
+  const userData = localStorage.getItem("user");
+  const parsedUser: {
+    id: string;
+    username: string;
+    name: string;
+    type: string;
+  } = JSON.parse(userData || "{}");
 
-  let showedPage
-  if (parsedUser.type === 'admin') showedPage = adminPage
-  else if (parsedUser.type === 'manajer') showedPage = managerPage
-  else showedPage = kasirPage
+  let showedPage;
+  if (parsedUser.type === "admin") showedPage = adminPage;
+  else if (parsedUser.type === "manajer") showedPage = managerPage;
+  else showedPage = kasirPage;
 
   const navigationButtons = showedPage.map((p) => (
     <Button variant="link" onClick={() => navigateTo(`/${p.target}`)}>
@@ -89,25 +106,29 @@ export default function LayoutBase({
   return (
     <div className="md:grid grid-cols-10">
       <div className="col-span-2 hidden md:block border-e">
-        <p className="font-bold text-xl my-4 px-8 pt-8">Kafe</p>
+        <p className="font-semibold text-2xl my-4 px-8 pt-8 font-serif">Cafe Bisa Ngopi</p>
         <div className="px-8 pb-8">
-        <Badge>{parsedUser.type}</Badge>
+          <Badge>{parsedUser.type}</Badge>
         </div>
         <Separator />
-        <div className="p-4 grid gap-2">{navigationButtons}</div>
+        <div className="p-4 grid gap-2">
+          {navigationButtons}
+          <Button variant="link" onClick={() => {setTheme(currentTheme === "light" ? "dark" : "light"); setCurrentTheme(currentTheme === "light" ? "dark" : "light")}}>
+            {currentTheme === "light" ? (<Moon/>):(<Sun/>)} <span className="ms-4">{currentTheme === "light" ? "Mode gelap" : "mode Terang"}</span>
+          </Button>
+        </div>
       </div>
       <div className="h-screen col-span-8 mt-16 md:mt-0">
-        <ScrollArea className="h-full p-4 md:p-8">
-        {children}
-        </ScrollArea>
+        <ScrollArea className="h-full p-4 md:p-8">{children}</ScrollArea>
       </div>
       <div className="absolute w-full top-0 bg-white/50 gap-2 p-2 md:hidden">
-      <Accordion type="single" collapsible>
-      
+        <Accordion type="single" collapsible>
           <AccordionItem value="item-1">
             <AccordionTrigger></AccordionTrigger>
             <AccordionContent className="grid backdrop-blur-sm">
-              <span className="text-center bg-green-500/20 rounded-sm text-green-800 font-bold py-2">{parsedUser.type}</span>
+              <span className="text-center bg-green-500/20 rounded-sm text-green-800 font-bold py-2">
+                {parsedUser.type}
+              </span>
               {navigationButtons}
             </AccordionContent>
           </AccordionItem>
